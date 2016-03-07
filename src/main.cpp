ed125ab07923c6d7199da9c6e6312771ad945096
Tim Officer <timothyofficer@gmail.com>
2016-03-07 18:23:08 -0500

    compile a filter expression for sniffing

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,11 @@ pcap_t& create_session(const char *device) {
 	int to_ms = 1000;
 	char *error_buffer;
 
+	struct bpf_program fp;		/* The compiled filter expression */
+	char filter_exp[] = "port 23";	/* The filter expression */
+	bpf_u_int32 mask;		/* The netmask of our sniffing device */
+	bpf_u_int32 net;		/* The IP of our sniffing device */
+
 	pcap_t* session = pcap_open_live(device, buffer_size, promisc, to_ms, error_buffer);
 
 	if(session == NULL)
@@ -38,6 +43,18 @@ pcap_t& create_session(const char *device) {
 		printf("Error Buffer: %s\n", error_buffer);
 
 		throw "Error opening session to device";
+	}
+
+	if (pcap_compile(session, &fp, filter_exp, 0, net) == -1) {
+		fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(session));
+
+		throw "Couldn't parse filter %s: %s\n", pcap_geterr(session);
+	}
+
+	if (pcap_setfilter(session, &fp) == -1) {
+		fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(session));
+
+		throw "Couldn't install filter %s: %s\n", pcap_geterr(session);
 	}
 
 	return *session;
