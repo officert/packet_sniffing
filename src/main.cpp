e01495b8ef8c831b441bde240641e2a98276d740
Tim Officer <timothyofficer@gmail.com>
2016-03-07 17:25:15 -0500

    change create_session to return a reference type

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,11 @@
 #include <pcap.h>
 
 //function prototypes
-pcap_t* create_session(const char *device);
+pcap_t& create_session(const char *device);
 
 int main(int argc, char *argv[])
 {
 	 char *device = argv[1];
-	 pcap_t* session_handle;
 
 	 if (device == NULL)
 	 {
@@ -17,14 +16,14 @@ int main(int argc, char *argv[])
 		 return(1);
    }
 
-	 session_handle = create_session(device);
+	 pcap_t& session_handle = create_session(device);
 
 	 printf("Device: %s\n", device);
 
 	 return(0);
 }
 
-pcap_t* create_session(const char *device) {
+pcap_t& create_session(const char *device) {
 	int buffer_size = BUFSIZ;
 	int promisc = 1;
 	int to_ms = 1000;
@@ -32,5 +31,5 @@ pcap_t* create_session(const char *device) {
 
 	pcap_t* session = pcap_open_live(device, buffer_size, promisc, to_ms, error_buffer);
 
-	return session;
+	return *session;
 }
