882bfbb9a356a5b482cb9786664a4e33bb049543
Tim Officer <timothyofficer@gmail.com>
2016-03-07 18:04:49 -0500

    stuff

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,27 @@
 #include <stdio.h>
 #include <pcap.h>
 
+using namespace std;
+
 //function prototypes
 pcap_t& create_session(const char *device);
 
 int main(int argc, char *argv[])
 {
-	 char *device = argv[1];
+	char *device = argv[1];
 
-	 if (device == NULL)
-	 {
-     std::cout << "Error, must pass a device name" << std::endl;
+	if (device == NULL)
+	{
+	 cout << "Error, must pass a device name" << endl;
 
-		 return(1);
-   }
+	 return(1);
+	}
 
-	 pcap_t& session_handle = create_session(device);
+	pcap_t& session_handle = create_session(device);
 
-	 printf("Device: %s\n", device);
+	printf("Device: %s\n", device);
 
-	 return(0);
+	return(0);
 }
 
 pcap_t& create_session(const char *device) {
@@ -30,6 +32,13 @@ pcap_t& create_session(const char *device) {
 	char *error_buffer;
 
 	pcap_t* session = pcap_open_live(device, buffer_size, promisc, to_ms, error_buffer);
+
+	if(session == NULL)
+	{
+		printf("Error Buffer: %s\n", error_buffer);
+
+		throw "Error opening session to device";
+	}
 
 	return *session;
 }
