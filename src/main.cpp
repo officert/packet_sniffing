a7d549645efb700ce23416caefa70970711c20f7
Tim Officer <timothyofficer@gmail.com>
2016-03-10 14:49:41 -0500

    sniff for a packet

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 //function prototypes
 pcap_t* create_session(const char *device);
+void sniff_session(pcap_t *session);
 
 int main(int argc, char *argv[])
 {
@@ -31,12 +32,19 @@ int main(int argc, char *argv[])
 		return(1);
 	}
 
-	printf("Device: %s\n", device);
+	printf("Sniffing on device: %s\n", device);
+
+	try {
+		sniff_session(session_handle);
+	} catch (const char* exception) {
+		cout << "Error, " << exception << endl;
+	}
 
 	return(0);
 }
 
-pcap_t* create_session(const char *device) {
+pcap_t* create_session(const char *device)
+{
 	int buffer_size = BUFSIZ;
 	int promisc = 1;
 	int to_ms = 1000;
@@ -56,12 +64,14 @@ pcap_t* create_session(const char *device) {
 		throw "Error opening session to device";
 	}
 
+	//create and compile a filter for only packets on port 23
 	if (pcap_compile(session, &fp, filter_exp, 0, net) == -1) {
 		fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(session));
 
 		throw "Couldn't parse filter %s: %s\n", pcap_geterr(session);
 	}
 
+	//set the filter to actually apply it to our current session
 	if (pcap_setfilter(session, &fp) == -1) {
 		fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(session));
 
@@ -69,4 +79,22 @@ pcap_t* create_session(const char *device) {
 	}
 
 	return session;
+}
+
+void sniff_session(pcap_t *session)
+{
+	struct pcap_pkthdr header;	/* The header that pcap gives us */
+	const u_char *packet;		/* The actual packet */
+
+	if (session == NULL)
+	{
+		throw "Error, session cannot be null";
+	}
+
+	// grab a packet
+	packet = pcap_next(session, &header);
+
+	printf("Jacked a packet with length of [%d]\n", header.len);
+
+	pcap_close(session);
 }
