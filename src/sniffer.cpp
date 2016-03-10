78792d3d451da41b537d34dd1bb9f48528acfaec
Tim Officer <timothyofficer@gmail.com>
2016-03-10 17:23:15 -0500

    fancy logging

--- src/sniffer.cpp
+++ src/sniffer.cpp
@@ -7,9 +7,11 @@
 using namespace std;
 
 //function prototypes
-void get_device_name(const char *device);
+void get_device_name(const char *device, bpf_u_int32 mask, bpf_u_int32 net, char *error_buffer);
+void log(const char *message);
 pcap_t* create_session(const char *device);
 void sniff_session(pcap_t *session);
+void fancy_printf(char* fmt, ...);
 
 void Sniffer::sniff(const char *device)
 {
@@ -28,7 +30,7 @@ void Sniffer::sniff(const char *device)
 		return;
 	}
 
-	printf("Sniffing on device: %s\n", device);
+	printf("\nSniffing on device: %s\n", device);
 
 	try {
 		sniff_session(session_handle);
@@ -39,16 +41,13 @@ void Sniffer::sniff(const char *device)
   return;
 }
 
-void get_device_name(const char *device)
+void get_device_name(const char *device, bpf_u_int32 mask, bpf_u_int32 net, char *error_buffer)
 {
   if (device == NULL) {
-    // if (pcap_lookupnet(device, &net, &mask, error_buffer) == -1)
-    // {
-    //   fprintf(stderr, "Can't get netmask for device %s\n", dev);
-    //   net = 0;
-    //   mask = 0;
-    // }
-    //device_name = NULL;
+    if (pcap_lookupnet(device, &net, &mask, error_buffer) == -1)
+    {
+      fprintf(stderr, "Can't get netmask for device %s\n", device);
+    }
   }
 }
 
@@ -59,21 +58,23 @@ pcap_t* create_session(const char *device)
 	int to_ms = 1000;
 	char *error_buffer;
 
-  get_device_name(device);
+  struct bpf_program fp;		/* The compiled filter expression */
+  char filter_exp[] = "port 23";	/* The filter expression */
+  bpf_u_int32 mask;		/* The netmask of our sniffing device */
+  bpf_u_int32 net;		/* The IP of our sniffing device */
 
-  printf("Device Name %s\n", device);
+  get_device_name(device, mask, net, error_buffer);
+
+  fancy_printf("Device Name %s\n", device);
+  fancy_printf("Net %s\n", net);
+  fancy_printf("Mask %s\n", mask);
 
   if(device == NULL)
   {
     throw "Error, could not get device name";
   }
 
-  printf("Using device: %s\n", device);
-
-	struct bpf_program fp;		/* The compiled filter expression */
-	char filter_exp[] = "port 23";	/* The filter expression */
-	bpf_u_int32 mask;		/* The netmask of our sniffing device */
-	bpf_u_int32 net;		/* The IP of our sniffing device */
+  printf("\nUsing device: %s\n", device);
 
 	pcap_t* session = pcap_open_live(device, buffer_size, promisc, to_ms, error_buffer);
 
@@ -116,7 +117,29 @@ void sniff_session(pcap_t *session)
 	// grab a packet
 	packet = pcap_next(session, &header);
 
-	printf("Jacked a packet with length of [%d]\n", header.len);
+	fancy_printf("Jacked a packet with length of [%d]\n", header.len);
 
 	pcap_close(session);
+}
+
+void fancy_printf(char* fmt, ...)
+{
+  char start[] = "\n/----------------------------------------\n\n";
+  char end[] = "\n----------------------------------------/\n";
+  int result_length = strlen(start) + strlen(fmt) + strlen(end);
+  char result[result_length];
+
+  strcpy(result, start);
+  strcat(result, fmt);
+  strcat(result, end);
+
+  fmt = result;
+
+  va_list args;
+
+  va_start(args,fmt);
+
+  vprintf(fmt,args);
+
+  va_end(args);
 }
